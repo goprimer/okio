255e3388ae2d77d345e346f4d52fa3be13aeccbf
cpetzel <craig.petzel@gmail.com>
2017-05-09 13:26:17 -0700

    removing non android stuff

--- okio/src/main/java/okio/DeflaterSink.java
+++ okio/src/main/java/okio/DeflaterSink.java
@@ -17,7 +17,7 @@ package okio;
 
 import java.io.IOException;
 import java.util.zip.Deflater;
-import org.codehaus.mojo.animal_sniffer.IgnoreJRERequirement;
+//import org.codehaus.mojo.animal_sniffer.IgnoreJRERequirement;
 
 import static okio.Util.checkOffsetAndCount;
 
@@ -79,7 +79,7 @@ public final class DeflaterSink implements Sink {
     }
   }
 
-  @IgnoreJRERequirement
+//  @IgnoreJRERequirement
   private void deflate(boolean syncFlush) throws IOException {
     Buffer buffer = sink.buffer();
     while (true) {
--- okio/src/main/java/okio/Okio.java
+++ okio/src/main/java/okio/Okio.java
@@ -25,12 +25,8 @@ import java.io.InterruptedIOException;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
-import java.nio.file.Files;
-import java.nio.file.OpenOption;
-import java.nio.file.Path;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.codehaus.mojo.animal_sniffer.IgnoreJRERequirement;
 
 import static okio.Util.checkOffsetAndCount;
 
@@ -167,11 +163,11 @@ public final class Okio {
   }
 
   /** Returns a source that reads from {@code path}. */
-  @IgnoreJRERequirement // Should only be invoked on Java 7+.
+  /*@IgnoreJRERequirement // Should only be invoked on Java 7+.
   public static Source source(Path path, OpenOption... options) throws IOException {
     if (path == null) throw new IllegalArgumentException("path == null");
     return source(Files.newInputStream(path, options));
-  }
+  }*/
 
   /** Returns a sink that writes to {@code file}. */
   public static Sink sink(File file) throws FileNotFoundException {
@@ -186,11 +182,11 @@ public final class Okio {
   }
 
   /** Returns a sink that writes to {@code path}. */
-  @IgnoreJRERequirement // Should only be invoked on Java 7+.
+  /*@IgnoreJRERequirement // Should only be invoked on Java 7+.
   public static Sink sink(Path path, OpenOption... options) throws IOException {
     if (path == null) throw new IllegalArgumentException("path == null");
     return sink(Files.newOutputStream(path, options));
-  }
+  }*/
 
   /** Returns a sink that writes nowhere. */
   public static Sink blackhole() {
